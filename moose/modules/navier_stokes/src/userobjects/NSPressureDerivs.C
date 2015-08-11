7705e6e1c6c22f7d5d5730d1068ccfa1fe02c528
Andrew E Slaughter <andrew.slaughter@inl.gov>
2015-08-11 16:33:33 -0600

    Remove copy of MOOSE

--- moose/modules/navier_stokes/src/userobjects/NSPressureDerivs.C
+++
@@ -1,7 +0,0 @@
-/****************************************************************/
-/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
-/*                                                              */
-/*          All contents are licensed under LGPL V2.1           */
-/*             See LICENSE for full restrictions                */
-/****************************************************************/
-// All code for this templated class is in the header file.
--- moose/python/FactorySystem/__init__.py
+++
@@ -1,8 +0,0 @@
-from Factory import Factory
-from InputParameters import InputParameters
-from MooseObject import MooseObject
-from Parser import Parser
-from Warehouse import Warehouse
-from ParseGetPot import ParseGetPot
-
-__all__ = ['Factory', 'InputParameters', 'MooseObject', 'Parser', 'Warehouse', 'ParseGetPot']
